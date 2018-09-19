5a41abe43846a9bb26a8628f0bf67acbfcf93147
andrewhofmeister <hofmeister.andy@gmail.com>
2018-09-18 21:27:04 -0500

    fix views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,8 +9,8 @@ def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	output = ', '.join([q.question_text for q in latest_question_list])
 	loader.get_template('polls/index.html')
-		context = {
-			'latest_question_list':
+	context = {
+		'latest_question_list':
 	latest_question_list,
 		}
 	return HttpResponse(template.render(context, request))
